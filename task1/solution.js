5e54c8b5b9bdccd58ce372f31936a3bb1953ea11
Aleksei Vozhdaev <vozhdaev.al@gmail.com>
2019-09-02 09:25:49 +0300

    first task solution attempt 1

--- task1/solution.js
+++ task1/solution.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 
 process.stdin.resume();
 process.stdin.setEncoding("utf-8");
@@ -21,22 +22,46 @@ process.stdin.on("end", _ => {
 
 let readLine = () => inputString[currentLine++];
 
+const MAX_NUMBER = 10000;
+
+const isEven = n => n % 2 === 0;
+
+const isPrime = num => {
+  if (num === 2) return true;
+  if (num > 2 && isEven(num)) return false;
+  if (num < 2 && num > MAX_NUMBER) return false;
+  for (let i = 3; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
 // Complete the getPrimes function below.
-function getPrimes(n) {}
+function* getPrimes(n) {
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) yield i;
+  }
+}
 
 let main = () => {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(
+    process.env.OUTPUT_PATH || path.resolve(__dirname, "output.txt")
+  );
 
   const n = parseInt(readLine(), 10);
 
   let res = [];
   let gen = getPrimes(n);
 
+  console.time();
   for (let i = 0; i < n; i++) {
     let val = gen.next().value;
     if (val == undefined) break;
     res.push(val);
   }
+  console.timeEnd();
+
+  console.log(res);
 
   ws.write(res.join("\n") + "\n");
 
