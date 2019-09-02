5cdd43eb188136a93a25a4a6f53ee12c69430955
Aleksei Vozhdaev <vozhdaev.al@gmail.com>
2019-09-02 10:40:19 +0300

    complete second solution

--- task2/solution.js
+++ task2/solution.js
@@ -24,13 +24,71 @@ process.stdin.on("end", _ => {
 
 let readLine = () => inputString[currentLine++];
 
+const AVAILABLE_SHAPES = ["square", "rectangle", "circle", "triangle"];
+const PI = 3.14;
+
+const checkShape = shape => AVAILABLE_SHAPES.includes(shape);
+
+const validateShapes = shapes => {
+  if (Array.isArray(shapes) && shapes.length > 0) {
+    for (let shape of shapes) {
+      if (!checkShape(shape)) return false;
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const checkArraysEqualLength = (firstArray, secondArray) =>
+  Array.isArray(firstArray) &&
+  Array.isArray(secondArray) &&
+  firstArray.length === secondArray.length;
+
+const toFixed = value => value.toFixed(2);
+const getDoubled = value => value * value;
+const normalize = value => parseFloat(value);
+
+const getSquareArea = side => normalize(toFixed(getDoubled(side)));
+const getRectangleArea = (width, length) => normalize(toFixed(width * length));
+const getCircleArea = radius => normalize(toFixed(PI * getDoubled(radius)));
+const getTriangleArea = (base, height) =>
+  normalize(toFixed(0.5 * base * height));
+
 // Complete the calculateArea function below.
 // It returns a Promise which on success, returns area of the shape, and on failure returns [-1].
-let calculateArea = (shape, values) => {};
+let calculateArea = (shape, values) =>
+  new Promise((resolve, reject) => {
+    switch (shape) {
+      case "square":
+        return resolve(getSquareArea(...values));
+      case "rectangle":
+        return resolve(getRectangleArea(...values));
+      case "circle":
+        return resolve(getCircleArea(...values));
+      case "triangle":
+        return resolve(getTriangleArea(...values));
+      default:
+        return reject([-1]);
+    }
+  });
+
+const calcAreas = (shapes, values_arr) => {
+  const results = [];
+  for (let index in shapes) {
+    results.push(calculateArea(shapes[index], values_arr[index]));
+  }
+  return Promise.all(results);
+};
 
 // Complete the generateArea function below.
 // It returns a Promise which on success, returns an array of areas of all the shapes and on failure, returns [-1].
-let getAreas = (shapes, values_arr) => {};
+let getAreas = (shapes, values_arr) =>
+  new Promise((resolve, reject) => {
+    if (!validateShapes(shapes)) reject([-1]);
+    if (!checkArraysEqualLength(shapes, values_arr)) reject([-1]);
+    resolve(calcAreas(shapes, values_arr));
+  });
 
 let callCalculateArea = async (shapes, values) =>
   (await calculateArea(shapes[0], values[0]).catch(error => error)) instanceof
