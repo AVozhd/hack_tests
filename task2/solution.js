3607b311312e666ec06bdbd4f45b4e22786c32ae
Aleksei Vozhdaev <vozhdaev.al@gmail.com>
2019-09-02 09:47:47 +0300

    init second task

---
+++ task2/solution.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", _ => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map(str => str.trim());
+
+  main();
+});
+
+let readLine = () => inputString[currentLine++];
+
+// Complete the calculateArea function below.
+// It returns a Promise which on success, returns area of the shape, and on failure returns [-1].
+let calculateArea = (shape, values) => {};
+
+// Complete the generateArea function below.
+// It returns a Promise which on success, returns an array of areas of all the shapes and on failure, returns [-1].
+let getAreas = (shapes, values_arr) => {};
+
+let callCalculateArea = async (shapes, values) =>
+  (await calculateArea(shapes[0], values[0]).catch(error => error)) instanceof
+  Promise;
+
+let callGetAreas = (shapes, values) =>
+  getAreas(shapes, values).catch(error => error);
+
+function main() {
+  const ws = fs.createWriteStream(
+    process.env.OUTPUT_PATH || path.resolve(__dirname, "output.txt")
+  );
+
+  const n = parseInt(readLine(), 10);
+
+  let shapes = [];
+
+  for (let shapesItr = 0; shapesItr < n; shapesItr++) {
+    const shapesItem = readLine();
+    shapes.push(shapesItem);
+  }
+
+  let values = [];
+
+  for (let valuesItr = 0; valuesItr < n; valuesItr++) {
+    const valuesItem = readLine();
+    values.push(JSON.parse("[" + valuesItem + "]"));
+  }
+
+  if (callCalculateArea(shapes, values)) {
+    callGetAreas(shapes, values).then(res => {
+      ws.write(res.join("\n") + "\n");
+      ws.end();
+    });
+  } else {
+    console.error("calculateArea does not return a Promise.");
+  }
+}
